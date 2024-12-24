e34e733f2039456552de7719103cac55dcf4ce45
Michael Wall <michael@horswall.com>
2024-12-24 07:58:05 +0000

    Day 24 part 1

---
+++ src/aoc/d24/part1.py
@@ -0,0 +1,45 @@
+from aoc.helpers.decorators import timer
+
+
+def calculate_value(a, op, b):
+    if op == "AND":
+        return a & b
+    elif op == "OR":
+        return a | b
+    elif op == "XOR":
+        return a ^ b
+
+
+def solve(puzzle_input):
+    answer = 0
+    wires, connections = puzzle_input.split("\n\n")
+
+    values = {wire.split(": ")[0]: int(wire.split(": ")[1]) for wire in wires.split("\n")}
+    connections = [connection.replace("-> ", "").split(" ") for connection in connections.strip().split("\n")]
+
+    z_count = sum(1 for x, y, z, wire in connections if wire.startswith("z"))
+
+    while len([key for key in values.keys() if key.startswith("z")]) != z_count:
+        for a, op, b, result in connections:
+            if a in values and b in values:
+                values[result] = calculate_value(values[a], op, values[b])
+            else:
+                continue
+
+    z_keys = sorted([key for key in values.keys() if key.startswith("z")], reverse=True)
+    z_values = [values[key] for key in z_keys]
+    binary_string = "".join(map(str, z_values))
+    answer = int(binary_string, 2)
+
+    return answer
+
+
+@timer
+def main():
+    with open("input.txt", "r", encoding="utf-8") as file:
+        data = file.read()
+    print(f"\nAnswer: {solve(data)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/aoc/d24/test_24.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+import pytest
+
+current_dir = Path(__file__).parent.resolve()
+
+
+def test_part_1():
+    from aoc.d24 import part1
+
+    with open(current_dir / "sample1.txt", "r", encoding="utf-8") as file:
+        sample1 = file.read()
+    assert part1.solve(sample1) == 4
+
+    with open(current_dir / "sample2.txt", "r", encoding="utf-8") as file:
+        sample2 = file.read()
+    assert part1.solve(sample2) == 2024
+
+
+@pytest.mark.skip
+def test_part_2():
+    from aoc.d24 import part2
+
+    with open(current_dir / "sample1.txt", "r", encoding="utf-8") as file:
+        sample1 = file.read()
+    assert part2.solve(sample1) == 0
+
+
+if __name__ == "__main__":
+    test_part_1()
+    test_part_2()
